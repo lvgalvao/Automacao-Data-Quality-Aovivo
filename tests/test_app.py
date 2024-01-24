12add89190299950c3b93685113fa55052a5e7a3
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-24 14:24:20 -0300

    test: adicionar nosso primeiro teste funcional

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,19 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
+# Precismaos definir qual driver vamos utilizar
 driver = webdriver.Firefox()
 
 # Define um timeout implícito
 driver.set_page_load_timeout(5)  # 5 segundos
 
+# Vamos fazer uma tratativa de try-except de entrar na nossa página
 try:
     driver.get("http://localhost:8501")
-    
-    # Aguarda até que o título da página esteja presente
-    WebDriverWait(driver, 5).until(EC.title_contains("Validador de Schema de Excel"))
-    print("Página carregada com sucesso e o título está correto.")
-
+    print("Acessou a página com sucesso")
 except TimeoutException:
     print("Tempo de carregamento da página excedeu o limite.")
 finally:
     driver.quit()
+
+
