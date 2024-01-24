ce379e1157b25c9758eb4240336b65447847ffd0
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-24 16:57:45 -0300

    feat: adicionar subir excel

--- src/app.py
+++ src/app.py
@@ -5,3 +5,8 @@ st.set_page_config(
 )
 
 st.title("Insira o seu excel para validação")
+
+arquivo = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+
+if arquivo:
+    st.success("O schema do arquivo Excel está correto!")
