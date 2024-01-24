071563319877ebb2070f0d7a28f7cc62c8e2c62a
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-24 15:46:10 -0300

    feat: adicionar app rodando

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,28 @@
+import pytest
+import subprocess
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from time import sleep
 
-# Precismaos definir qual driver vamos utilizar
-driver = webdriver.Firefox()
+@pytest.fixture
+def driver():
+    # Iniciar o Streamlit em background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
 
-# Define um timeout implícito
-driver.set_page_load_timeout(5)  # 5 segundos
+    # Iniciar o WebDriver usando GeckoDriver
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    yield driver
 
-# Vamos fazer uma tratativa de try-except de entrar na nossa página
-try:
-    driver.get("http://localhost:8501")
-    print("Acessou a página com sucesso")
-except TimeoutException:
-    print("Tempo de carregamento da página excedeu o limite.")
-finally:
+    # Fechar o WebDriver e o Streamlit após o teste
     driver.quit()
+    process.kill()
 
+def test_app_opens(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    sleep(5)
 
+def test_app_opens(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    sleep(5)
